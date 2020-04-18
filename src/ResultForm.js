ed9083de22accdea9afa0eaf7677e8a4377718a6
Jennifer Beckingham <jenniferbeckingham@Jennifers-MacBook-Air.local>
2020-04-18 13:06:21 +0100

    Improve results select element

--- src/ResultForm.js
+++ src/ResultForm.js
@@ -1,10 +1,16 @@
 import React from 'react'
-import { Input, Form, Button, TextArea, Header, Table, Label } from 'semantic-ui-react'
+import { Form, Button, Header, Table, Label, Select } from 'semantic-ui-react'
 
 const getColour = i => {
     const colours = ['teal', 'pink', 'yellow', 'purple', 'orange', 'green']
     return colours[i % colours.length]
 }
+
+const resultOptions = [
+    { key: 'correct', value: 1, text: 'Correct' },
+    { key: 'half', value: 0.5, text: "I'll give ya half" },
+    { key: 'incorrect', value: 0, text: 'Incorrect' }
+  ]
 
 class ResultForm extends React.Component {
     constructor(props) {
@@ -15,12 +21,8 @@ class ResultForm extends React.Component {
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleChange(event) {
-        let newResults = this.state.results;
-        let playerName = event.target.name;
-        let answer = event.target.value;
-        newResults[event.target.name] = parseFloat(event.target.value);
-        this.setState({ results: newResults });
+    handleChange(value, name) {
+        this.setState({ results: { ...this.state.results, [name]: value} });
     }
 
     handleSubmit(event) {
@@ -36,24 +38,20 @@ class ResultForm extends React.Component {
 
     render() {
         return (<div>
-            <Form onSubmit={this.handleSubmit} size='massive' style={{ width: '50%', margin: 'auto' }}>
+            <Form onSubmit={this.handleSubmit} style={{ width: '50%', margin: 'auto' }}>
                 <Table basic="very">
                     <Table.Body>
                         {Object.keys(this.props.gameState.currentQuestion.answers).map((name, i) => (
                             <Table.Row key={name}>
                                 <Table.Cell>
-                                    <Label as="a" color={this.getPlayerColour(name)} size="huge" style={{ textAlign: "center" }}>{name}</Label>
+                                    <Label as="a" size="huge" color={this.getPlayerColour(name)} style={{ textAlign: "center" }}>{name}</Label>
                                 </Table.Cell>
                                 <Table.Cell>
-                                    <Header size="huge">{this.props.gameState.currentQuestion.answers[name]}</Header>
+                                    <Header size="huge" >{this.props.gameState.currentQuestion.answers[name]}</Header>
                                 </Table.Cell>
                                 <Table.Cell>
-                                    <Header size="huge">
-                                        <select name={name} value={this.state.value} onChange={this.handleChange}>
-                                            <option value="1">Correct</option>
-                                            <option value="0.5">I'll give ya half</option>
-                                            <option value="0">Incorrect</option>
-                                        </select>
+                                    <Header>
+                                       <Select onChange={ (event, data) => this.handleChange(data.value, name) } placeholder='Select...' options={resultOptions} />
                                     </Header>
                                 </Table.Cell>
                             </Table.Row>
